b127872a272c62674e52482293c64e5f6c32ebaa
Piyush Patidar <piyushpatidar5@gmail.com>
2020-09-15 19:13:15 +0530

    Added Auth

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
