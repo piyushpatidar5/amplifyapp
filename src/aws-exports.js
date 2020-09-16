fb89e0500167d0e88ab9041be2fa626e2a6fc7d1
Piyush Patidar <piyushpatidar5@gmail.com>
2020-09-16 11:41:52 +0530

    Added Graphql

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "ap-south-1",
     "aws_user_pools_id": "ap-south-1_HjE5IUmdQ",
     "aws_user_pools_web_client_id": "1vlft2oue6qpd43f8fmjjca0ad",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://elh5grksrfdidg32hmqhxlrwie.appsync-api.ap-south-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-south-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-x6qwmmei4zgqfdojxwszvbgkpq"
 };
 
 
